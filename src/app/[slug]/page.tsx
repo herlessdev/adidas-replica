8d9b9320303478405778a2fcc1f2bf5faaa0f9c2
AizenSousuke <kurono1807@gmail.com>
2025-06-12 10:53:29 -0500

    update

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,7 +3,8 @@ import { slugify } from "@/utils/slugify";
 import { notFound } from "next/navigation";
 import Product from "./product";
 
-export default async function ProductPage({ params }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function ProductPage({ params }: any) {
   const { slug } = await params;
   const product = articles.find((article) => slugify(article.name) === slug);
   const dictionary = await import(`@/locales/es.json`).then(
